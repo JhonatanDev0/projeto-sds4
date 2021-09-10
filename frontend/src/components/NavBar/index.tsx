dfcafe66ca69191036af22da87b6a38650e4bf98
Jhonatan Oliveira <jhonatandev0@gmail.com>
2021-09-10 20:55:34 -0300

    Pagination

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -7,7 +7,7 @@ function NavBar() {
             <div className="container">
                 <nav className="my-2 my-md-0 mr-md-3">
                     <Link to="/">
-                    <img src={ImgDsDark} alt="DevSuperior" width="120" />
+                        <img src={ImgDsDark} alt="DevSuperior" width="120" />
                     </Link>
                 </nav>
             </div>
